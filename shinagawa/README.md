a1dab2d6654d747f41872f46427a51a2ac12021e
Kenjiro HIGASHI <35302518+kken78@users.noreply.github.com>
2025-08-14 23:00:56 +0900

    chore: prepare shinagawa dir

--- shinagawa/README.md
+++
@@ -1,3 +0,0 @@
-# kken78.github.io
-# これは何？
-- https://kken78.github.io/ へのリンクを表示するために作成
--- shinagawa/fetch-logger.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const orig = window.fetch;
-  window.fetch = function(input, init) {
-    const url = typeof input === "string" ? input : input?.url;
-    if (url) console.debug("[fetch]", url);
-    return orig(input, init);
-  };
-})();
