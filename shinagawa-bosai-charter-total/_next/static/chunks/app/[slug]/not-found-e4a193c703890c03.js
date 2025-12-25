987535f634f716756b748ba1472427c1a2d05b48
Kenjiro HIGASHI <35302518+kken78@users.noreply.github.com>
2025-12-26 00:59:00 +0900

    amendment

--- shinagawa-bosai-charter-total/_next/static/chunks/app/[slug]/not-found-e4a193c703890c03.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[513],{18583:(e,s,n)=>{Promise.resolve().then(n.bind(n,38567)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[315,567,874,441,684,358],()=>s(18583)),_N_E=e.O()}]);
